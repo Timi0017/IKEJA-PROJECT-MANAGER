8e726aa0dcdea5c22cdeeaec79b0e1d1457e4332
Timi0017 <timilehinduroishola@gmail.com>
2026-06-23 10:27:43 +0100

    fixed the cors issue to avoid blockage of the render server

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app =FastAPI(title="Internal Project Manager API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://ikeja-project-manager.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
